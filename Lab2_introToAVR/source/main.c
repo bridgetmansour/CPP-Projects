35843b6160b8cb5a50dd0df35aa1c63ebd6f0829
Bridget Mansour <bmans001@ucr.edu>
2019-06-26 14:24:13 -0700

    Update main.c

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -15,7 +15,8 @@
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
-
+    unsigned char tmpB = 0x00;
+    unsigned char tmpA = 0x00;
     unsigned char temp_input = 0x00;
     while (1) {
         temp_input = PINA;
